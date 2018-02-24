739fa65b2d8437a2cad9fa062b3e18912d51a0a5
Ivan Chapman <irchap99@gmail.com>
2018-02-24 09:38:47 -0800

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Six dollar burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
